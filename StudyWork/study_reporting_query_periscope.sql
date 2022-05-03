04f6f83ae9b1fdf917ca72e26a538dfc7daa949e
Justin Thomson <justin.thomson@viome.com>
2022-05-03 12:57:45 -0400

    column name and order changes

--- StudyWork/study_reporting_query_periscope.sql
+++ StudyWork/study_reporting_query_periscope.sql
@@ -15,16 +15,16 @@ with notifications as (
 		when cft.id=16 then 7
 		when cft.id=18 then 8
 		when cft.id=19 then 9
-		end as study_id, count(distinct cfr.user_id) as DistinctUsersContacted
+		end as study_id, count(distinct cfr.user_id) as Users_Contacted
 	from chat_flow_request cfr
 	join ai_info.chat_flow_template cft on cft.id=cfr.chat_flow_template_id
 	group by 1
 ), 
 studsamples as (
 -- get breakdown of study samples by type
-	select study_id, count(distinct study_sample_id) filter (where st.sample_type_id=1) as StoolSamples,
-		count(distinct study_sample_id) filter (where st.sample_type_id=2) as BloodSamples,
-		count(distinct study_sample_id) filter (where st.sample_type_id=3) as SalivaSamples
+	select study_id, count(distinct study_sample_id) filter (where st.sample_type_id=1) as Stool_Samples,
+		count(distinct study_sample_id) filter (where st.sample_type_id=2) as Blood_Samples,
+		count(distinct study_sample_id) filter (where st.sample_type_id=3) as Saliva_Samples
 	from study_info.study_sample s
 	join study_info.sample_type st on s.sample_type_id=st.sample_type_id
 	where st.sample_type_id in (1,2,3) -- stool, blood, saliva
@@ -32,13 +32,13 @@ studsamples as (
 ),
 qanda as (
 -- get question and answer data 
-	select sf.study_id, count(distinct sf.feature_id) as QuestionCount, count(a.*) as AnswerCount 
+	select sf.study_id, count(distinct sf.feature_id) as Questions, count(a.*) as Answers 
 	from study_info.intervention_study_feature sf
 	left join study_info.response a on a.feature_id=sf.feature_id
 	group by sf.study_id
 ),
 baseinfo as (
-select s.study_id, s.name, s.description, count(distinct sp.participant_id) as Participant_Count
+select s.study_id, s.name, s.description, count(distinct sp.participant_id) as Participants
 from study_info.study s
 left join notifications nt on nt.study_id=s.study_id
 left join study_info.study_participant sp on s.study_id=sp.study_id
@@ -46,10 +46,11 @@ left join studsamples samp on samp.study_id=s.study_id
 group by s.study_id
 ) 
 
-select s.study_id, s.name, s.description, s.Participant_Count,
-	nt.DistinctUsersContacted as App_Users_Contacted_Via_Chat,
-	samp.StoolSamples, samp.BloodSamples, samp.SalivaSamples,
-	q.QuestionCount, q.AnswerCount 
+select s.study_id, s.name, s.description, 
+	nt.Users_Contacted,
+	s.Participants,
+	samp.Stool_Samples, samp.Blood_Samples, samp.Saliva_Samples,
+	q.Questions, q.Answers
 from baseinfo s
 left join notifications nt on nt.study_id=s.study_id
 left join studsamples samp on samp.study_id=s.study_id
