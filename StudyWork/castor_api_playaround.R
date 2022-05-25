075502af06d163cea836ae808598f8886e53bdd1
Justin Thomson <justin.thomson@viome.com>
2022-05-25 14:49:24 -0400

    updated study counting with 128.1 data

--- StudyWork/castor_api_playaround.R
+++ StudyWork/castor_api_playaround.R
@@ -7,6 +7,10 @@ install_github("castoredc/castoRedc")
 
 library(castoRedc)
 
-castor_api <- CastorData$new(key = Sys.getenv("CASTOR_KEY"), 
-                             secret = Sys.getenv("CASTOR_SECRET"), 
-                             base_url = "https://data.castoredc.com")
+BASE_URL="https://data.castoredc.com/"
+
+castor_api <- CastorData$new(key = Sys.getenv("CASTOR_KEY"), secret = Sys.getenv("CASTOR_SECRET"), base_url = BASE_URL)
+
+
+
+
--- StudyWork/v109.3/count_basic_info.R
+++ StudyWork/v109.3/count_basic_info.R
@@ -2,6 +2,7 @@
 #count basic info for 109.3 and 109.4 studies
 
 library(readxl)
+library(aws.s3)
 
 #109.3
 q109 = read.csv("/Users/justin/Downloads/1093ValidationStudyData/subject_metadata/Questionnaire_Blood_test_Measurement_ValidationStudy.csv")
@@ -14,10 +15,14 @@ print(answerCount)
 
 #109.4
 
+countAnswersinDF = function(sht, startRow=2, startCol=2) {
+  sum(!is.na(sht[startRow:nrow(sht), startCol:ncol(sht)]))
+}
+
 # take in excel questionnare and count total number of answers 
 countAnswers = function(sheetLoc) {
   sht = read_excel((sheetLoc))
-  cnt = sum(!is.na(sht[2:nrow(sht), 2:ncol(sht)]))
+  cnt = countAnswersinDF(sht)
   
   print(paste0(cnt, " answers for sheet ", sheetLoc))
   cnt
@@ -44,5 +49,31 @@ print(paste0("total answers for 109.4: ", b1Count+b2Count+b3Count))
 q112 = read.csv("/Users/justin/Downloads/v112_data.csv")
 answerCount112 = sum(!is.na(q112[2:nrow(q112), 2:ncol(q112)]))
 print(answerCount112)
+
+count
+#v128.1
+handle128p1 = function() {
+  bucket = "viome-studies"
+  v128b1="/v128.1/OC_Metadata_AUS_1st_batch_100_samples.xlsx"
+  v128b2="/v128.1/OC_Metadata_AUS_2nd_batch_30_samples.xlsx"
+  v128b3="/v128.1/OC_Metadata_AUS_3rd_batch_50_samples.xlsx"
+  v128b4="/v128.1/OC_Metadata_AUS_4th_batch_50_samples.xlsx"
+  v128b5="/v128.1/OC_Metadata_AUS_5th_batch_100_OPC_samples.xlsx"
+  
+  b1=s3read_using(FUN=read_excel, bucket = bucket, object = v128b1)
+  b2=s3read_using(FUN=read_excel, bucket = bucket, object = v128b2)
+  b3=s3read_using(FUN=read_excel, bucket = bucket, object = v128b3)
+  b4=s3read_using(FUN=read_excel, bucket = bucket, object = v128b4)
+  b5=s3read_using(FUN=read_excel, bucket = bucket, object = v128b5)
+  
+  # total number of participants
+  total_partcipants=nrow(b1)+nrow(b2)+nrow(b3)+nrow(b4)+nrow(b5)
+  print(paste0("total 128.1 participants: ", total_partcipants))
+  
+  # 12 total questions. Some sheets have smaller amount
+  # 
+  total_answers = countAnswersinDF(b1, startRow = 1, startCol = 3) + countAnswersinDF(b2, startRow = 1, startCol = 3) + countAnswersinDF(b3, startRow = 1, startCol = 3)  + countAnswersinDF(b4, startRow = 1, startCol = 3) + countAnswersinDF(b5, startRow = 1, startCol = 3) 
+  print(paste0("total number of 128.1 questions: 12, answers: ", total_answers))
+}
 
 
