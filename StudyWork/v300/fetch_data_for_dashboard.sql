4da775e4ac97b1b52e3789a3be13291412bff18d
Justin Thomson <justin.thomson@viome.com>
2022-06-20 13:51:05 -0400

    updated dashboard code

--- StudyWork/v300/fetch_data_for_dashboard.sql
+++ StudyWork/v300/fetch_data_for_dashboard.sql
@@ -78,8 +78,10 @@ FROM field_data_table
 GROUP BY record_id
 )
 select record_id as "Castor Patient ID", screen_eligibility as "Screen Eligibility", saliva_collection_date1 as "Date of Saliva Collection", labss_date_collection as "Date Saliva Sample Received", nymc_oc_status as "Oral/Throat Cancer Status", nymc_oc_type as "Type of Oral Cancer", 
-case when (nymc_oc_status='Negative' AND lesions in ('Dysplasia','Hyperplasia','Erythroplakia (abnormal red lesions in your mouth)','Lichenoid lesions','Actinic Keratosis')) then 'OPMD-M' 
-	when (nymc_oc_status='Positive' AND lesions in ('Dysplasia','Hyperplasia','Erythroplakia (abnormal red lesions in your mouth)','Lichenoid lesions','Actinic Keratosis')) then 'OC-OPMD-M' 
+case when (nymc_oc_status='Negative' AND lesions in ('Dysplasia','Hyperplasia','Erythroplakia (abnormal red lesions in your mouth)','Leukoplakia (thick white patches in your mouth and gums)','Lichenoid lesions')) then 'OPMD-M' 
+	when (nymc_oc_status in ('Positive', 'Unknown') AND lesions in ('Dysplasia','Hyperplasia','Erythroplakia (abnormal red lesions in your mouth)','Leukoplakia (thick white patches in your mouth and gums)','Lichenoid lesions')) then 'OC-OPMD-M' 
+ 	when (nymc_oc_status='Negative' AND lesions in ('Aphthous ulcer / Canker sores (ulcers in your mouth)','Gingival enlargement (side effect)','Lichen planus (white lacy patches, red or swollen tissue, open sore)','Keratosis','Inflammatory reaction','Cheek bites','Dry mouth')) then 'OPMD-B'
+	when (nymc_oc_status in ('Positive', 'Unknown') AND lesions in ('Aphthous ulcer / Canker sores (ulcers in your mouth)','Gingival enlargement (side effect)','Lichen planus (white lacy patches, red or swollen tissue, open sore)','Keratosis','Inflammatory reaction','Cheek bites','Dry mouth')) then 'OC-OPMD-B'
 	else '' end as "OPMD",
 nymc_oc_hpv as "HPV Status"
 from subject_report
