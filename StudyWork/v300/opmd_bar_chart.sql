4662b2af39dac910cb84fe2ca40e1202f110aa94
Justin Thomson <justin.thomson@viome.com>
2022-06-22 11:21:10 -0400

    separate unknown and update logic for positive cases

--- StudyWork/v300/opmd_bar_chart.sql
+++ StudyWork/v300/opmd_bar_chart.sql
@@ -16,21 +16,17 @@ field_data_table as (
 subject_report as (
 SELECT
     record_id,
-    MAX(value) FILTER (WHERE field = 'screen_eligibility') AS "screen_eligibility",
-    MAX(value) FILTER (WHERE field = 'saliva_collection_date1') AS "saliva_collection_date1",
-    MAX(value) FILTER (WHERE field = 'labss_date_collection') AS "labss_date_collection",
     MAX(value) FILTER (WHERE field = 'lesions') AS "lesions",
     MAX(value) FILTER (WHERE field = 'nymc_oc_status') AS "nymc_oc_status",
-    MAX(value) FILTER (WHERE field = 'nymc_oc_type') AS "nymc_oc_type",
-    MAX(value) FILTER (WHERE field = 'nymc_oc_hpv') AS "nymc_oc_hpv"
+    MAX(value) FILTER (WHERE field = 'nymc_oc_type') AS "nymc_oc_type"
 FROM field_data_table
 GROUP BY record_id
 )
 select 
-case when (nymc_oc_status='Negative' AND lesions in ('Dysplasia','Hyperplasia','Erythroplakia (abnormal red lesions in your mouth)','Leukoplakia (thick white patches in your mouth and gums)','Lichenoid lesions')) then 'OPMD-M' 
-	when (nymc_oc_status in ('Positive', 'Unknown') AND lesions in ('Dysplasia','Hyperplasia','Erythroplakia (abnormal red lesions in your mouth)','Leukoplakia (thick white patches in your mouth and gums)','Lichenoid lesions')) then 'OC-OT' 
+case when nymc_oc_status = 'Positive' then 'OC-OT'
+	when nymc_oc_status = 'Unknown' then 'Unknown'
+	when (nymc_oc_status='Negative' AND lesions in ('Dysplasia','Hyperplasia','Erythroplakia (abnormal red lesions in your mouth)','Leukoplakia (thick white patches in your mouth and gums)','Lichenoid lesions')) then 'OPMD-M' 
  	when (nymc_oc_status='Negative' AND lesions in ('Aphthous ulcer / Canker sores (ulcers in your mouth)','Gingival enlargement (side effect)','Lichen planus (white lacy patches, red or swollen tissue, open sore)','Keratosis','Inflammatory reaction','Cheek bites','Dry mouth')) then 'OPMD-B'
-	when (nymc_oc_status in ('Positive', 'Unknown') AND lesions in ('Aphthous ulcer / Canker sores (ulcers in your mouth)','Gingival enlargement (side effect)','Lichen planus (white lacy patches, red or swollen tissue, open sore)','Keratosis','Inflammatory reaction','Cheek bites','Dry mouth')) then 'OC-OT'
 	else 'Healthy' end as "OPMD",
 1 as ycolumn
 from subject_report
